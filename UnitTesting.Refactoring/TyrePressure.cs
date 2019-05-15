c0d61b5b2c8c79cba92c6bfacbee123e48674bd7
Mindaugas Šeduikis <mseduikis@alna.lt>
2019-05-15 08:52:42 +0300

    exercises updates

---
+++ UnitTesting.Refactoring/TyrePressure.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTesting.Refactoring
+{
+    public class TyrePressure
+    {
+        public double Psi { get; set; }
+        public DateTime CheckedOn { get; set; }
+    }
+}
---
+++ UnitTesting.Twitter/CoffeeMachine.cs
@@ -0,0 +1,20 @@
+﻿using UnitTesting.Twitter.Interfaces;
+
+namespace UnitTesting.Twitter
+{
+    public class CoffeeMachine : ITwitterClient
+    {
+        private readonly IWriter _writer;
+        private readonly ITweetRepository _tweetRepository;
+
+        public CoffeeMachine(IWriter writer, ITweetRepository tweetRepository)
+        {
+            _writer = writer;
+            _tweetRepository = tweetRepository;
+        }
+
+        public void WriteTweet(string message) => _writer.WriteLine(message);
+
+        public void SendTweetToServer(string message) => _tweetRepository.SaveTweet(message);
+    }
+}
