48027c9924a477567208bc0e03475387a12421cb
Mindaugas Šeduikis <mseduikis@alna.lt>
2019-05-15 08:54:08 +0300

    exercises updates

--- UnitTesting.Refactoring/Alarm.cs
+++ UnitTesting.Refactoring/Alarm.cs
@@ -1,9 +1,15 @@
-﻿namespace UnitTesting.Refactoring
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace UnitTesting.Refactoring
 {
     public class Alarm
     {
         private const double LowPressureThreshold = 17;
         private const double HighPressureThreshold = 21;
+        private const string OutputFilePath = @"C:\logs.json";
 
         private readonly Sensor _sensor = new Sensor();
         private readonly OnBoardComputer _onBoardComputer = new OnBoardComputer();
@@ -25,8 +31,24 @@
                 AlarmOn = true;
                 _onBoardComputer.ShowWarning($"Dangerous tyre pressure: {psiPressureValue}");
             }
+            else
+            {
+                _onBoardComputer.ShowInfo($"Tyre pressure: {psiPressureValue}");
+            }
 
-            _onBoardComputer.ShowInfo($"Tyre pressure: {psiPressureValue}");
+            var tyrePressure = new TyrePressure
+            {
+                Psi = psiPressureValue,
+                CheckedOn = DateTime.UtcNow
+            };
+
+            var jsonData = File.Exists(OutputFilePath) ? File.ReadAllText(OutputFilePath) : string.Empty;
+            var tyrePressureList =
+                JsonConvert.DeserializeObject<List<TyrePressure>>(jsonData) ?? new List<TyrePressure>();
+            tyrePressureList.Add(tyrePressure);
+            jsonData = JsonConvert.SerializeObject(tyrePressureList);
+            File.WriteAllText(OutputFilePath, jsonData);
+
             LastTyrePressure = psiPressureValue;
         }
     }
--- UnitTesting.Twitter/IO/ConsoleWriter.cs
+++ UnitTesting.Twitter/IO/ConsoleWriter.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using UnitTesting.Twitter.Interfaces;
 
 namespace UnitTesting.Twitter.IO
 {
-    public class ConsoleWriter
+    public class ConsoleWriter : IWriter
     {
         public void WriteLine(string message) => Console.WriteLine(message);
     }
