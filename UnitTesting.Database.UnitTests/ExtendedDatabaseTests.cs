ef297811d87ee8d780131ca85c67afb017582f90
Mindaugas Šeduikis <mseduikis@alna.lt>
2019-05-22 10:59:47 +0300

    slides

---
+++ UnitTesting.Database.UnitTests/ExtendedDatabaseTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnitTesting.Database.Repository;
+using Xunit;
+
+namespace UnitTesting.Database.UnitTests
+{
+    public class ExtendedDatabaseTests
+    {
+        [Fact]
+        public void GivenExtendedDatabase_WhenFindWithNegativeId_ThenThrowsException()
+        {
+            //Arrange
+            var id = -1;
+            var database = new ExtendedDatabase();
+
+            //Act
+            //Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => database.Find(id));
+        }
+    }
+}
---
+++ UnitTesting.Twitter.UnitTests/TweetTests.cs
@@ -0,0 +1,27 @@
+﻿using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnitTesting.Twitter.Interfaces;
+using Xunit;
+
+namespace UnitTesting.Twitter.UnitTests
+{
+    public class TweetTests
+    {
+        [Fact]
+        public void GivenTweet_WhenReceivedMessage_ThenWriteToClient() {
+
+            //Arrange
+            var twitterClientMock = Substitute.For<ITwitterClient>();
+            var tweet = new Tweet(twitterClientMock);
+            string message = "Test message";
+
+            //Act
+            tweet.ReceiveMessage(message);
+
+            //Assert
+            twitterClientMock.Received(1).WriteTweet(Arg.Is<string>(message));
+        }
+    }
+}
